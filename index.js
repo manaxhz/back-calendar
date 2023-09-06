ddae4b7adfcd04fe5c10f9e2b9146360a07533e3
Gonzalo Aracena <gonzalo.aracena.p@gmail.com>
2023-09-06 11:44:43 -0300

    Backend 1.0 terminado Correcion PORT

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.use('/api/events', require('./routes/events'));
 
 
 // Escuchar peticiones
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 });
